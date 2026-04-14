e786cff1df018e54e888e58a4526f5d7cfb872b1
juangiraaaldo <juangiraaaldo10@gmail.com>
2026-04-13 23:59:46 -0500

    jumm

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Taller3-React.github.io/'
+  base: '/Taller3React.github.io/'
 })
